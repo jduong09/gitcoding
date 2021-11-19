0e123baba679d7bb3d278dcdfa7bb7625b964c7d
ziggysauce <dtn3423@gmail.com>
2021-11-19 01:28:49 -0500

    setup basic postgresql db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('./db');
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,30 @@ app.listen(port, () => {
   console.log(`App running on port ${port}.`);
 });
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  await db.createUsers();
   res.send('Server is setup :)');
+});
+
+app.get('/users', async (req, res) => {
+  console.log('FETCHING USERS');
+  const { data, error } = await db.getAllUsers();
+  console.log('RETURNED DATA: ', data, error);
+  if(error) {
+    res.status(400).json({ error });
+  } else {
+    res.status(200).json({ data });
+  }
+});
+
+app.post('/users', async (req, res) => {
+  const { name } = req.body;
+  console.log('ADDING NEW USER: ', name);
+  const { data, error } = await db.insertUser(name);
+  console.log('RETURNED DATA: ', data, error);
+  if(error) {
+    res.status(400).json({ error });
+  } else {
+    res.status(200).json({ data });
+  }
 });
