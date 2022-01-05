84c1517d113026347778e2289d7dddc04d29ab77
Justin Duong <jduong@umich.edu>
2022-01-05 18:02:28 -0500

    Moved static serving up.

--- server/server.js
+++ server/server.js
@@ -32,6 +32,17 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/** 
+ * @description Serve static files from express backend.
+*/
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(process.cwd(), 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'client', 'build', 'index.html'));
+  });
+}
 
 /*
  * Session Configuration
@@ -111,11 +122,3 @@ const checkAuthentication = (req, res, next) => {
 app.use('/users/:userId', checkAuthentication, (req, res, next) => {
   next();
 });
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(process.cwd(), 'client', 'build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(process.cwd(), 'client', 'build', 'index.html'));
-  });
-}
