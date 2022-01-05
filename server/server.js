461046741840bb51f0692f65045cb7b76bb291e1
Justin Duong <jduong@umich.edu>
2022-01-04 20:29:02 -0500

    Fixed 404 not found error.

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/src", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/public", "index.html"));
 })
 
 /*
