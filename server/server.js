96bf0b9694185bb11f75b0348e19d416e416faf8
Justin Duong <jduong@umich.edu>
2022-01-04 19:01:27 -0500

    Changed port number.

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const apiRouter = require('./api');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
