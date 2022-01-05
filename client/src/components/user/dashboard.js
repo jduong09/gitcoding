84bd9d7d5c64789039d15270e71c1fd3d8d8def8
ziggysauce <dtn3423@gmail.com>
2022-01-05 00:13:15 -0500

    fix frontend urls

--- client/src/components/user/dashboard.js
+++ client/src/components/user/dashboard.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
+const href = process.env.NODE_ENV === 'production' ? '/auth/logout' : 'http://localhost:5000/auth/logout';
+
 const Dashboard = () => (
   <div>
     <header>
       <h1>Hello this is the users dashboard page.</h1>
-      <a href="/auth/logout">
+      <a href={href}>
         Sign Out!
         <FontAwesomeIcon icon={faSignOutAlt} />
       </a>
