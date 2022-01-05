fa1f3b4959a673d618bcd340564b1b7254fd40b6
ziggysauce <dtn3423@gmail.com>
2022-01-04 23:19:30 -0500

    auth js headers

--- server/api/routes/auth.js
+++ server/api/routes/auth.js
@@ -6,6 +6,16 @@ dotenv.config();
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 /** 
  * Routes Definitions
 */
