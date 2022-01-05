fcbbb412d36b60dbce22929d9919dcfd83991f76
ziggysauce <dtn3423@gmail.com>
2022-01-05 00:23:35 -0500

    add CORS preflight to server.js

--- server/server.js
+++ server/server.js
@@ -19,6 +19,11 @@ app.listen(PORT, () => {
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
