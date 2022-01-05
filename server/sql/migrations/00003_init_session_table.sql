ca7adaf30845bb0a7edba1c1e94850676ea254f1
Justin Duong <jduong@umich.edu>
2022-01-05 15:31:28 -0500

    Changed session table to sessions table.

--- server/sql/migrations/00003_init_session_table.sql
+++ server/sql/migrations/00003_init_session_table.sql
@@ -1,10 +1,10 @@
-CREATE TABLE "session" (
+CREATE TABLE IF NOT EXISTS "sessions" (
   "sid" varchar NOT NULL COLLATE "default",
 	"sess" json NOT NULL,
 	"expire" timestamp(6) NOT NULL
 )
 WITH (OIDS=FALSE);
 
-ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+ALTER TABLE "sessions" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
 
-CREATE INDEX "IDX_session_expire" ON "session" ("expire");
+CREATE INDEX "IDX_session_expire" ON "sessions" ("expire");
