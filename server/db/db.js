dbf3fe96af40a40dacb61011d9fdba6de389d80b
Justin Duong <jduong@umich.edu>
2022-01-04 20:24:32 -0500

    Added rejectUnauthorized option.

--- server/db/db.js
+++ server/db/db.js
@@ -6,7 +6,12 @@ const { getOutStandingMigrations } = require('../api/actions/migrations');
 
 dotenv.config();
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({ 
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 pool.on('connect', () => {
   console.log('Connected to the db!');
