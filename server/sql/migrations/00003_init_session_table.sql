47c483f976e3f3550a53da9e6f570aee37c667d8
Justin Duong <jduong@umich.edu>
2022-01-07 11:50:40 -0500

    Added spacing.

--- server/sql/migrations/00003_init_session_table.sql
+++ server/sql/migrations/00003_init_session_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS "sessions" (
-"sid" varchar NOT NULL COLLATE "default",
-"sess" json NOT NULL,
-"expire" timestamp(6) NOT NULL
+	"sid" varchar NOT NULL COLLATE "default",
+	"sess" json NOT NULL,
+	"expire" timestamp(6) NOT NULL
 )
 WITH (OIDS=FALSE);
 
