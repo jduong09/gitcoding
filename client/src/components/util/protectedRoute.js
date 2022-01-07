50723227015b18ca418c737203508be4075b9d41
Justin Duong <jduong@umich.edu>
2022-01-06 19:48:42 -0500

    Deleted util folder.

--- client/src/components/util/protectedRoute.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const ProtectedRoute = ({ children, isAuth }) =>  isAuth ? children : <Navigate to="/" />
-
-export default ProtectedRoute;
