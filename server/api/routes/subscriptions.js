7fcdf8aaef02496bb46a2060eed9d7846ebd2f0a
Justin Duong <jduong@umich.edu>
2022-01-11 09:26:29 -0500

    Changed subscription routing.

--- server/api/routes/subscriptions.js
+++ server/api/routes/subscriptions.js
@@ -5,20 +5,53 @@ const { createSubscription } = require('../actions/subscriptions');
 // under the route /users/:userId/subscriptions/
 const router = express.Router();
 
-// Read all user's subscriptions
-// Route: /users/:userId/subscriptions
-router.get(async (req, res) => {
-  try {
-    const data = await getSubscriptionsByUser();
-    res.status(200).json(data);
-  } catch(error) {
-    res.status(400).json(error);
-  }
-});
+router.route('/')
+  // Read all user's subscriptions
+  // Route: /users/:userId/subscriptions
+  .get(async (req, res) => {
+    try {
+      const data = await getSubscriptionsByUser();
+      res.status(200).json(data);
+    } catch(error) {
+      res.status(400).json(error);
+    }
+  })
+  // CREATE a subscription
+  // Route: /users/:userId/subscriptions/new
+  .post(async (req, res) => {
+    // req.body will contain the information necessary to make a new subscription
+    try {
+      const data = await createSubscription(req.body);
+      res.status(200).json(data);
+    } catch(error) {
+      res.status(400).json(error);
+    }
+  })
+  // Update a subscription
+  // Route: /users/:userId/:subscriptionsId
+  .patch(async (req, res) => {
+    try {
+      // req.body contains the information needed to update the subscription.
+      const data = await updateSubscription(req.body);
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  })
+  // Delete a subscription
+  // Route: /users/:userId/subscriptions/:subscriptionId
+  .delete(async(req, res) => {
+    try {
+      const data = await deleteSubscription();
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  });
 
 // READ a single user subscription
 // Route: /users/:userId/subscriptions/:subscriptionId
-router.get(async (req, res) => {
+router.get('/:subscriptionId', async (req, res) => {
   try {
     // execute db query getSubscriptions.
     const data = await getSubscriptionById();
@@ -29,41 +62,6 @@ router.get(async (req, res) => {
     // If query encounters an error, 
     // Issue a 400 status, and place error message in body as JSON 
     res.status(400).json(error)
-  }
-});
-
-// CREATE a subscription
-// Route: /users/:userId/subscriptions/new
-router.post('/new', async (req, res) => {
-  // req.body will contain the information necessary to make a new subscription
-  try {
-    const data = await createSubscription(req.body);
-    res.status(200).json(data);
-  } catch(error) {
-    res.status(400).json(error);
-  }
-});
-
-// Update a subscription
-// Route: /users/:userId/:subscriptionsId
-router.put(async (req, res) => {
-  try {
-    // req.body contains the information needed to update the subscription.
-    const data = await updateSubscription(req.body);
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(400).json(error);
-  }
-});
-
-// Delete a subscription
-// Route: /users/:userId/subscriptions/:subscriptionId
-router.delete(async(req, res) => {
-  try {
-    const data = await deleteSubscription();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(400).json(error);
   }
 });
 
