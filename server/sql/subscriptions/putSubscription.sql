dd891d66c59d17dbdcd17e0d919c570604f700c4
Justin Duong <jduong@umich.edu>
2022-01-26 16:45:24 -0500

    Ran npm install to add react-day-picker. Added dueDate to return statement in SQL put/patch files.

--- server/sql/subscriptions/putSubscription.sql
+++ server/sql/subscriptions/putSubscription.sql
@@ -1,3 +1,3 @@
 INSERT INTO subscriptions(name, nickname, due_date, reminder_days, amount, user_id)
 VALUES (NULLIF(${name}, ''), NULLIF(${nickname}, ''), ${dueDate}, ${reminderDays}, ${amount}, ${userId})
-RETURNING name, nickname, reminder_days AS "reminderDays", amount, subscription_uuid AS "subscriptionUuid";
+RETURNING name, nickname, due_date AS "dueDate", reminder_days AS "reminderDays", amount, subscription_uuid AS "subscriptionUuid";
