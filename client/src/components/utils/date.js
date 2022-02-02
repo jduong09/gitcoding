a54d8084ae81e7d5e89761e17c5274d1e062eb35
Justin Duong <jduong@umich.edu>
2022-02-02 09:04:36 -0500

    Added checks for todays date. If array contains today date, set due date to today.

--- client/src/components/utils/date.js
+++ client/src/components/utils/date.js
@@ -3,43 +3,51 @@ import { DateUtils } from 'react-day-picker';
 const displayDueDate = ({ frequency, occurence, dates }) => {
   const todaysDate = new Date();
   const firstDate = new Date(dates[0]);
-  let nearestDueDate = firstDate < todaysDate ?  DateUtils.addMonths(firstDate, 12) : firstDate;
+  let nearestDueDate = firstDate;
   let dueDateString = '';
-
   // Yearly: If date is before current date, then we set due date to following year.
   // If date is after current date, then we set due date to given date.
   if (frequency === 'yearly') {
-    // if date is after current date, then we set dueDate to given date.
-    dates.map((date) => {
-      const dateObject = new Date(date);
+    nearestDueDate = firstDate < todaysDate ?  DateUtils.addMonths(firstDate, 12) : firstDate;
+    for (let i = 0; i < dates.length; i += 1) {
+      const dateObject = new Date(dates[i]);
+      // if dates contains today, dueDate is today.
+      if (DateUtils.isSameDay(todaysDate, dateObject)) {
+        nearestDueDate = todaysDate;
+        break;
+      }
+
       if (dateObject > todaysDate) {
         if (dateObject < nearestDueDate) {
-          nearestDueDate = new Date(date);
+          nearestDueDate = new Date(dates[i]);
         } 
-      };
-      return nearestDueDate;
-    });
+      }
+    }
 
     dueDateString = (nearestDueDate > todaysDate) 
       ? `${nearestDueDate.toLocaleDateString()}, Yearly` 
       : `${DateUtils.addMonths(nearestDueDate, 12).toLocaleDateString()}, Yearly`;
   } else if (frequency === 'monthly') {
-    dates.map((date) => {
-      const dateObject = new Date(date);
+    for (let i = 0; i < dates.length; i += 1) {
+      const dateObject = new Date(dates[i]);
 
-      
+      if (DateUtils.isSameDay(todaysDate, dateObject)) {
+        nearestDueDate = todaysDate;
+        break;
+      }
+
       if (dateObject > todaysDate) {
         if (dateObject < nearestDueDate) {
-          nearestDueDate = new Date(date);
+          nearestDueDate = new Date(dates[i]);
         }
-      };
-      return nearestDueDate;
-    });
-
+      }
+    }
     // if occurence is greater than 1, we need to set reminder for the current month as well.
-    dueDateString = (occurence > 1) 
+    dueDateString = (occurence > 1 && nearestDueDate < todaysDate) 
       ? `${DateUtils.addMonths(nearestDueDate, parseInt(occurence, 10)).toLocaleDateString()}, Monthly`
       : `${nearestDueDate.toLocaleDateString()}, Monthly`;
+  } else if (frequency === 'weekly') {
+    console.log(todaysDate.getDay());
   }
   return dueDateString;
 
