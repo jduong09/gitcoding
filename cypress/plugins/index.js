e41f808848f35cac96a90a877503cd945f42e568
Justin Duong <jduong@umich.edu>
2022-03-17 18:10:51 -0400

    Added port env to cypress.env config.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -23,10 +23,11 @@ dotenv.config()
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  config.env.clientId = process.env.CLIENT_ID
-  config.env.clientSecret = process.env.CLIENT_SECRET
-  config.env.domain = process.env.ISSUER
-  config.env.callbackUrl = process.env.CALLBACK_URL
+  config.env.CLIENT_ID = process.env.CLIENT_ID
+  config.env.CLIENT_SECRET = process.env.CLIENT_SECRET
+  config.env.DOMAIN = process.env.ISSUER
+  config.env.CALLBACK_URL = process.env.CALLBACK_URL
+  config.env.PORT = process.env.PORT
 
   return config
 }
