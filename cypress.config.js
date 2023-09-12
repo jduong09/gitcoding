034616197a94051014926405207d78d4f63a97d6
Justin Duong <jduong@umich.edu>
2023-09-12 18:23:02 -0400

    Added supportFile false option to negate error.

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000'
+    baseUrl: 'http://localhost:3000',
+    supportFile: false
   },
   env: {
     CLIENT_ID: process.env.CLIENT_ID,
@@ -11,5 +12,5 @@ module.exports = defineConfig({
     DOMAIN: process.env.ISSUER,
     CALLBACK_URL: process.env.CALLBACK_URL,
     PORT: process.env.PORT
-  }
+  },
 });
