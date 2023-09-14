df161e03ecb30f01b1131b306f55ec0880a6f32d
Justin Duong <jduong@umich.edu>
2023-09-14 14:24:12 -0400

    Added two small tests for landing page and changed node engines to run on node 16 and npm engine to run on 8.0

--- cypress/e2e/home_page_spec.js
+++ cypress/e2e/home_page_spec.js
@@ -1,13 +1,16 @@
-describe('The Home Page', () => {
+describe('The Landing Page', () => {
   it('successfully loads', () => {
     cy.visit('/');
   });
 
-  /*
-  it('allows you to sign in', () => {
-    cy.contains('Sign In');
+  it('contains header one', () => {
+    cy.visit('/');
+    cy.get('main h1',{withinSubject:null}).should('have.text', 'Keep Your Subscriptions Healthy ');
   });
-  */
 
+  it('contains sign in button', () => {
+    cy.visit('/');
+    cy.get('[data-cy="sign-in"]',{withinSubject:null}).should('have.text', 'Sign In');
+  });
   // TODO: May need some mock data to simulate for tests
 });
